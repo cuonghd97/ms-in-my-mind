251b518434d7d61d8c9c105c11ccf86b03c70f0d
cuonghoangduc <cuonghoangduc>
2023-07-20 15:39:17 +0700

    update openapi generator

--- user-service/src/main/java/dev/thesemicolon/userservice/apis/AuthenticationController.java
+++ user-service/src/main/java/dev/thesemicolon/userservice/apis/AuthenticationController.java
@@ -9,11 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import the.semicolon.userservice.client.api.AuthenticationApi;
 
 
 @RestController
 @RequestMapping("api/authentication")
-public class AuthenticationController {
+public class AuthenticationController implements AuthenticationApi {
     private AuthenticationService authenticationService;
 
     @Autowired
